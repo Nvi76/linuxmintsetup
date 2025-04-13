1061443fef8683f516bddfd8c00fba8f643199f5
oriole1 <nvme12@proton.me>
2025-04-13 15:17:07 +0700

    Update cliapps.sh

--- cliapps.sh
+++ cliapps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
- curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && sudo apt install flatpak build-essential nautilus-extension-gnome-terminal && brew install gcc && sudo apt install gnome-software-plugin-flatpak && flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo apt install build-essential nautilus-extension-gnome-terminal && curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" 
