a8c7c2a18f3a623a024da4e6f636568b73d24018
oriole1 <nvme12@proton.me>
2025-04-19 17:28:21 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-sudo apt install git nala curl && sudo apt update && sudo apt upgrade && sudo snap install thunderbird firefox snap-store && chmod +x secure.sh && chmod +x ollama.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && ./cliapps.sh && ./secure.sh && ./apps.sh
+https://github.com/oriole1/ubuntusetup/edit/main/start.sh
+sudo apt install git nala curl && sudo apt update && sudo apt upgrade  && chmod +x secure.sh && chmod +x ollama.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && ./cliapps.sh && ./secure.sh && ./apps.sh
