857ac5f8bc81d1036efa1b5df2e305d1f9fb2aca
Chorusn <nvme12@proton.me>
2025-05-04 07:38:02 +0700

    Update cliapps.sh

--- cliapps.sh
+++ cliapps.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-sudo apt install nala build-essential nautilus-extension-gnome-terminal && curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && sudo apt-get install build-essential
+sudo nala install build-essential nautilus-extension-gnome-terminal -y && curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh && /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" && sudo apt-get install build-essential -y
