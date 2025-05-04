394c437b67e38d60a77a1d7347fe1c9671585bcc
Chorusn <nvme12@proton.me>
2025-05-04 14:19:33 +0700

    Delete start.sh

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo apt install git nala curl -y && sudo apt update -y && sudo apt upgrade -y && chmod +x secure.sh && chmod +x de.sh && chmod +x ollama.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && ./de.sh && ./cliapps.sh && ./secure.sh && ./apps.sh
