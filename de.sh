a741c086b39809a2e47d537dc9514d6b0ba0e9df
Chorusn <nvme12@proton.me>
2025-05-04 07:59:32 +0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-chmod +x apps.sh && chmod +x cliapps.sh && chmod +x secure.sh && chmod +x ollama.sh && chmod +x updater.sh && sudo apt update -y && sudo apt upgrade -y && sudo snap refresh && sudo snap install firefox thunderbird && sudo apt install nala -y && sudo nala install nvidia-driver-550 ubuntu-gnome-desktop ubuntu-desktop plymouth plymouth-themes* network-manager -y && sudo systemctl start NetworkManager.service && sudo systemctl enable NetworkManager.service && ./cliapps.sh && ./apps.sh && sudo update-alternatives --config default.plymouth && sudo nano /etc/gdm3/custom.conf && sudo nano /etc/default/grub && sudo update-grub && 
+sudo apt update -y && sudo apt upgrade -y && sudo snap refresh && sudo snap install firefox thunderbird && sudo apt install nala -y && sudo nala install nvidia-driver-550 ubuntu-gnome-desktop ubuntu-desktop plymouth plymouth-themes* network-manager -y && sudo systemctl start NetworkManager.service && sudo systemctl enable NetworkManager.service && ./cliapps.sh && ./apps.sh && sudo update-alternatives --config default.plymouth && sudo nano /etc/gdm3/custom.conf && sudo nano /etc/default/grub && sudo update-grub && 
 sudo update-initramfs -u
 
 
