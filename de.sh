c04f8917c68726f62ed7d357d9ac0bb9da280de0
chorusn <nvme12@proton.me>
2025-05-29 14:23:24 +0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -9,5 +9,19 @@ chmod +x secure.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapp
 sudo apt update -y && sudo apt upgrade -y 
 
 # Updating and installing core snap apps
-sudo snap refresh && sudo snap install firefox thunderbird snap-center && sudo apt install git curl nala -y && sudo nala install ubuntu-gnome-desktop ubuntu-desktop plymouth plymouth-themes* network-manager -y && sudo systemctl start NetworkManager.service && sudo systemctl enable NetworkManager.service && sudo systemctl disable systemd-networkd-wait-online.service && sudo systemctl disable NetworkManager-wait-online.service && sudo update-alternatives --config default.plymouth && sudo nano /etc/gdm3/custom.conf && sudo nano /etc/default/grub && sudo update-grub && 
-sudo update-initramfs -u
+sudo snap refresh && sudo snap install firefox thunderbird snap-center 
+
+# Installing necesseary packages
+sudo apt install figlet git curl nala -y
+
+# Installing the desktop
+sudo nala install ubuntu-gnome-desktop ubuntu-desktop plymouth plymouth-themes* network-manager -y 
+
+# Enabling services
+sudo systemctl start NetworkManager.service && sudo systemctl enable NetworkManager.service && sudo systemctl disable systemd-networkd-wait-online.service && sudo systemctl disable NetworkManager-wait-online.service 
+
+# Setting default plymouth themes
+sudo update-alternatives --config default.plymouth 
+
+# Applying changes to grub and initramfs
+sudo nano /etc/gdm3/custom.conf && sudo nano /etc/default/grub && sudo update-grub && sudo update-initramfs -u
