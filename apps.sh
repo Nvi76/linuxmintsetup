c1a189c2901c342224366e68c54f7507133a9a99
chorusn <nvme12@proton.me>
2025-06-03 12:29:50 +0700

    Update apps.sh

--- apps.sh
+++ apps.sh
@@ -12,8 +12,8 @@ sudo snap refresh && sudo snap install code --classic && sudo snap install codiu
 wget -O- https://updates.signal.org/desktop/apt/keys.asc | gpg --dearmor > signal-desktop-keyring.gpg &&
 cat signal-desktop-keyring.gpg | sudo tee /usr/share/keyrings/signal-desktop-keyring.gpg > /dev/null && echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main' |\ sudo tee /etc/apt/sources.list.d/signal-xenial.list
 
-# Downloading warp.dev and safing portmaster and anytype
-curl https://releases.warp.dev/stable/v0.2025.04.30.08.11.stable_01/warp-terminal_0.2025.04.30.08.11.stable.01_amd64.deb --output warp-term.deb && curl https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb --output portmaster.deb && curl https://anytype-release.fra1.cdn.digitaloceanspaces.com/anytype_0.46.8_amd64.deb --output anytype.deb
+# Downloading safing portmaster and anytype
+curl https://updates.safing.io/latest/linux_amd64/packages/portmaster-installer.deb --output portmaster.deb && curl https://anytype-release.fra1.cdn.digitaloceanspaces.com/anytype_0.46.8_amd64.deb --output anytype.deb
 
 # Importing brave browser's repo
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg && echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list 
@@ -22,13 +22,13 @@ sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://b
 sudo apt update -y && sudo apt upgrade -y 
 
 # Installing deb packages
-sudo nala install flatpak neovim kakoune signal-desktop gnome-software-plugin-flatpak gnome-shell-extension-manager gnome-weather gnome-tweaks gnome-maps gnome-calendar build-essential ubuntu-restricted-extras timeshift libreoffice brave-browser gufw ufw -y 
+sudo nala install flatpak neovim kitty kakoune nautilus-extension-gnome-terminal signal-desktop gnome-software-plugin-flatpak gnome-shell-extension-manager gnome-weather gnome-tweaks gnome-maps gnome-calendar build-essential ubuntu-restricted-extras timeshift libreoffice brave-browser gufw ufw -y 
 
 # 40%
 figlet 40% Complete
 
 # Installing locally Downloaded deb packages
-sudo nala install ./warp-term.deb -y && sudo nala install ./portmaster.deb -y && sudo nala install ./anytype.deb
+sudo nala install ./portmaster.deb -y && sudo nala install ./anytype.deb
 
 # Importing flathub's repo
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
