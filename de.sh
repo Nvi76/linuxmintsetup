6f16a420df746713c548adbf43438cf24b1cf3a7
chorusn <nvme12@proton.me>
2025-06-03 12:35:31 +0700

    Update de.sh

--- de.sh
+++ de.sh
@@ -2,20 +2,20 @@
 
 set -euo pipefail
 
-# Elevating file permissions
+# Elevate file permissions
 chmod +x secure.sh && chmod +x updater.sh && chmod +x apps.sh && chmod +x cliapps.sh && chmod +x ollama.sh
 
-# Updating the syste,
+# Updates the system
 sudo apt update -y && sudo apt upgrade -y 
 
-# Updating and installing core snap apps
+# Update and install core snap apps
 sudo snap refresh && sudo snap install firefox thunderbird snap-center 
 
 # Installing necesseary packages
 sudo apt install figlet git curl nala -y
 
-# Installing the desktop
-sudo nala install ubuntu-gnome-desktop ubuntu-desktop plymouth plymouth-themes* network-manager -y 
+# Installing the desktop and other essential apps
+sudo nala install ubuntu-gnome-desktop ubuntu-desktop network-manager fwupd plymouth plymouth-themes* -y 
 
 # Enabling services
 sudo systemctl start NetworkManager.service && sudo systemctl enable NetworkManager.service && sudo systemctl disable systemd-networkd-wait-online.service && sudo systemctl disable NetworkManager-wait-online.service 
